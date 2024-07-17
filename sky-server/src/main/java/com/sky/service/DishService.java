8c5665097017e110de7ce373f2b8a0988cbb3182
coderxguan <3288733739@qq.com>
2024-07-17 17:02:25 +0800

    菜品分页查询业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,4 +15,7 @@ public interface DishService {
 
     // 新增菜品
     public void saveWithFlavor(DishDTO dishDTO);
+
+    // 菜品分页查询
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
